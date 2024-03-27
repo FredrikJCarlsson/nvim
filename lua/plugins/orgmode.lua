6be1a6dbc49afffc2a9e72308b4441d637c572f2
Fredrik Carlsson <fredrik.j.carlsson@unipower.se>
2024-03-27 09:14:06 +0100

    Improved org mode config

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -21,8 +21,20 @@ return{
 
     -- Setup orgmode
     require('orgmode').setup({
-      org_agenda_files = '~/orgfiles/**/*',
-      org_default_notes_file = '~/orgfiles/refile.org',
+      org_agenda_files = {'C:/Users/fredrik.j.carlsson/OneDrive - Unipower AB/OrgMode/*'},
+      org_default_notes_file = 'C:/Users/fredrik.j.carlsson/OneDrive - Unipower AB/OrgMode/refile.org',
     })
+
+
+    -- Load custom treesitter grammar for org filetype
+    require('orgmode').setup_ts_grammar()
+
+    -- Treesitter configuration
+    require('nvim-treesitter.configs').setup {
+      highlight = {
+        enable = true,
+      },
+      ensure_installed = {'org'}, -- Or run :TSUpdate org
+    }
   end, 
 }
