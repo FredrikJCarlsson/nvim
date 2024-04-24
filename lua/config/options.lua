4d3792eb5e7f0d61ec57a4bc96065c324edac027
Fredrik Carlsson <fredrik.j.carlsson@unipower.se>
2024-04-24 11:05:28 +0200

    reverted clipboard code

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,14 +4,5 @@
 local opt = vim.opt
 opt.shiftwidth = 4 -- Size of an indent
 
-
 opt.clipboard = "unnamedplus"
 
-if vim.fn.has('wsl') == 1 then
-    vim.api.nvim_create_autocmd('TextYankPost', {
-    group = vim.api.nvim_create_augroup('Yank', { clear = true }),
-    callback = function()
-        vim.fn.system('clip.exe', vim.fn.getreg('"'))
-    end,
-    })
-end
