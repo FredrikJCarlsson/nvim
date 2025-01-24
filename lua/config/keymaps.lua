a0667b49a684d36d1ab30c772c1b6e96f8314daa
FredrikCarlsson <fredrik.carlsson.92@gmail.com>
2025-01-24 21:05:03 +0100

    feat: add Xcodebuild plugin and keymaps
    - Added Xcodebuild plugin configuration in lua/plugins/xcodebuild.lua
    - Added keymaps for Xcodebuild actions in lua/config/keymaps.lua
    - Removed debugger and lsp plugin configurations
    - Updated .gitignore to include lazyvim.json

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,29 @@ keymap.set("n", "<C-w><left>", "<C-w><")
 keymap.set("n", "<C-w><right>", "<C-w>>")
 keymap.set("n", "<C-w><up>", "<C-w>+")
 keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Xcodebuild
+vim.keymap.set("n", "<leader>Z", "<cmd>XcodebuildPicker<cr>", { desc = "Show Xcodebuild Actions" })
+vim.keymap.set("n", "<leader>zf", "<cmd>XcodebuildProjectManager<cr>", { desc = "Show Project Manager Actions" })
+
+vim.keymap.set("n", "<leader>zb", "<cmd>XcodebuildBuild<cr>", { desc = "Build Project" })
+vim.keymap.set("n", "<leader>zB", "<cmd>XcodebuildBuildForTesting<cr>", { desc = "Build For Testing" })
+vim.keymap.set("n", "<leader>zr", "<cmd>XcodebuildBuildRun<cr>", { desc = "Build & Run Project" })
+
+vim.keymap.set("n", "<leader>zt", "<cmd>XcodebuildTest<cr>", { desc = "Run Tests" })
+vim.keymap.set("v", "<leader>zt", "<cmd>XcodebuildTestSelected<cr>", { desc = "Run Selected Tests" })
+vim.keymap.set("n", "<leader>zT", "<cmd>XcodebuildTestClass<cr>", { desc = "Run Current Test Class" })
+vim.keymap.set("n", "<leader>z.", "<cmd>XcodebuildTestRepeat<cr>", { desc = "Repeat Last Test Run" })
+
+vim.keymap.set("n", "<leader>zl", "<cmd>XcodebuildToggleLogs<cr>", { desc = "Toggle Xcodebuild Logs" })
+vim.keymap.set("n", "<leader>zc", "<cmd>XcodebuildToggleCodeCoverage<cr>", { desc = "Toggle Code Coverage" })
+vim.keymap.set("n", "<leader>zC", "<cmd>XcodebuildShowCodeCoverageReport<cr>", { desc = "Show Code Coverage Report" })
+vim.keymap.set("n", "<leader>ze", "<cmd>XcodebuildTestExplorerToggle<cr>", { desc = "Toggle Test Explorer" })
+vim.keymap.set("n", "<leader>zs", "<cmd>XcodebuildFailingSnapshots<cr>", { desc = "Show Failing Snapshots" })
+
+vim.keymap.set("n", "<leader>zd", "<cmd>XcodebuildSelectDevice<cr>", { desc = "Select Device" })
+vim.keymap.set("n", "<leader>zp", "<cmd>XcodebuildSelectTestPlan<cr>", { desc = "Select Test Plan" })
+vim.keymap.set("n", "<leader>zq", "<cmd>Telescope quickfix<cr>", { desc = "Show QuickFix List" })
+
+vim.keymap.set("n", "<leader>zz", "<cmd>XcodebuildQuickfixLine<cr>", { desc = "Quickfix Line" })
+vim.keymap.set("n", "<leader>za", "<cmd>XcodebuildCodeActions<cr>", { desc = "Show Code Actions" })
