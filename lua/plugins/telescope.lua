4e85efe5055718e129af1c6e2bbcea787b5ae4d9
FredrikCarlsson <fredrik.carlsson.92@gmail.com>
2025-12-20 12:38:32 +0100

    Fixed telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,16 @@
-return {
-  -- Add Telescope plugin
-  {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+-- return {
+--   -- Add Telescope plugin
+--   {
+--     'nvim-telescope/telescope.nvim',
+--     dependencies = { 'nvim-lua/plenary.nvim' },
+--   },
+-- }
+-- in your lazyvim config or a separate plugin config file
+require('snacs').setup({
+  telescope = {
+    defaults = {
+      hidden = true,    -- include hidden files
+      no_ignore = true, -- include .gitignored files
+    },
   },
-}
+})
