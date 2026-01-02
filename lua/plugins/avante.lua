e8eb0a94c895e95f89e043b576a55b80f90f3af5
FredrikCarlsson <fredrik.carlsson.92@gmail.com>
2026-01-02 11:29:03 +0100

    Fixed avante

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,18 @@
+return {
+    "yetone/avante.nvim",
+    opts = {
+        provider = "codex",
+        selection = {
+            hint_display = "delayed",
+        },
+        behaviour = {
+            auto_set_keymaps = true,
+        },
+        acp_providers = {
+            ["codex"] = {
+                command = "npx",
+                args = { "@zed-industries/codex-acp" },
+            },
+        },
+    },
+}
