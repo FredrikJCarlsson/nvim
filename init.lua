5d091c18115e1d7e37a2f35fbaa42653b1d2bee5
Fredrik Carlsson <fredrik.j.carlsson@unipower.se>
2026-01-21 09:29:49 +0100

    Fixed clipboard

--- init.lua
+++ init.lua
@@ -1,7 +1,26 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.g.autoformat = false
+vim.o.clipboard = "unnamedplus"
 
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
 
 -- require('snacks').setup {
 --   pickers = {
